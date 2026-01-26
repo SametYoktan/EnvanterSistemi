932bd747515092a563a44856b25adffaf6f8c302
Samet YOKTAN <yoktan81@gmail.com>
2026-01-26 16:42:40 +0300

    AnaSayfa Sayfa Geçişi

--- EnvanterSistemi/Program.cs
+++ EnvanterSistemi/Program.cs
@@ -39,7 +39,7 @@ namespace EnvanterSistemi
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Urun}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
