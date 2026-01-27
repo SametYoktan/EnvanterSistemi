f6268918cf94c107657b510768be7d07a0b22291
Samet YOKTAN <yoktan81@gmail.com>
2026-01-27 13:40:05 +0300

    Siparis Controller Bitti RD,Detay Yapılıyor.

--- EnvanterSistemi/Controllers/SiparisController.cs
+++ EnvanterSistemi/Controllers/SiparisController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnvanterSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace EnvanterSistemi.Controllers
+public class SiparisController : Controller
 {
-    public class SiparisController : Controller
+    private readonly EnvanterContext _context;
+    public SiparisController(EnvanterContext context)
     {
-        public IActionResult Index()
+        _context = context;
+    }
+
+    // Listeleme
+    public async Task<IActionResult> Index()
+    {
+        var siparisler = await _context.Siparislers
+            .Include(s => s.SiparisDetaylars)
+                .ThenInclude(sd => sd.Urun)
+            .ToListAsync();
+        return View(siparisler);
+    }
+
+
+    // GET: Delete Onay Sayfası
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var siparis = await _context.Siparislers
+            .FirstOrDefaultAsync(m => m.SiparisId == id);
+
+        if (siparis == null) return NotFound();
+
+        return View(siparis);
+    }
+
+    // POST: Gerçek Silme İşlemi
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var siparis = await _context.Siparislers.FindAsync(id);
+        if (siparis != null)
         {
-            return View();
+            _context.Siparislers.Remove(siparis);
+            await _context.SaveChangesAsync();
         }
+
+        return RedirectToAction(nameof(Index));
     }
 }
